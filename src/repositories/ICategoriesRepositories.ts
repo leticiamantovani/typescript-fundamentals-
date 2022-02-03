76d7ddb93fc34589a2e80a58b8d8e4b7caa6138b
Leticia Mantovani <leticiamantovani159@gmail.com>
2022-02-03 00:58:31 -0300

    implementing solid principles

---
+++ src/repositories/ICategoriesRepositories.ts
@@ -0,0 +1,13 @@
+import { Category } from '../model/category';
+
+interface ICreateCategoryDTO {
+  name: string,
+  description: string;
+}
+
+interface ICategoriesRepository {
+  findByName(name: string): Category;
+  list(): Category[];
+  create({ name, description }:ICreateCategoryDTO): void;
+}
+export { ICategoriesRepository, ICreateCategoryDTO };
