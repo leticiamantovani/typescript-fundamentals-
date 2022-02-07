57d5a16f08c397adc14a1fce418569c199fb5493
Leticia Mantovani <leticiamantovani159@gmail.com>
2022-02-07 01:13:45 -0300

    implementing SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { CreateCategoryServive } from '../services/CreateCategoryService';
+import { CategoriesRepository } from '../../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryServive } from '../../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
---
+++ src/routes/specification.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import { SpecificationRepository } from '../../modules/cars/repositories/SpecificationRepository';
+import { CreateSpecificationService } from '../../modules/cars/services/CreateSpecificationService';
+
+const specificationRoutes = Router();
+
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+  const createpecificationService = new CreateSpecificationService(specificationRepository);
+
+  createpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationRoutes };
